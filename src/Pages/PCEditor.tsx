80e37f01dcee827b9380887904690543a6e142a2
dabucz <76279444+dabucz@users.noreply.github.com>
2023-07-29 20:21:15 +0300

    added db

--- src/Pages/PCEditor.tsx
+++ src/Pages/PCEditor.tsx
@@ -9,6 +9,7 @@ import Cards from '../Components/Cards';
 import LeftBar from '../Components/LeftBar';
 import RightBar from '../Components/RightBar';
 import PCNavbar from '../Components/PCNavbar';
+import { getRoutePathById } from '../router';
 
 import { Data } from '../Utils/Props';
 
